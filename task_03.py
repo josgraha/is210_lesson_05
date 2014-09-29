b3fc9d34980d9f400efaab59745c01268e74804e
Joseph Graham <josgraha@gmail.com>
2014-09-29 16:38:05 -0400

    better error handling task03

--- task_03.py
+++ task_03.py
@@ -8,19 +8,32 @@ def celsius_to_fahrenheit(temperature):
 def fahrenheit_to_celsius(temperature):
     return (5 * (temperature - 32)) / 9.0
 
+def is_format_valid(temp_format):
+    if temp_format is None:
+        return False
+    elif temp_format.lower() not in ['c', 'f']:
+        return False
+    return True
+
 def convert_temperature(temperature, output_format='c'):
     temp = temperature
     given_input = None
     input_format = 'f'
+    if not is_format_valid(output_format):
+        return None
+    else:
+        output_format = output_format.lower()
     try:
         float(temperature)
     except ValueError:
-        given_input = 'f' if temperature.endswith('f') else 'c'
+        given_input = temperature.lower()[-1:]
         try:
             temp = float(temperature[:-1])
         except ValueError:
             return None
     if given_input is not None:
+        if not is_format_valid(given_input):
+            return None
         input_format = given_input
     else:
         input_format = 'c' if output_format.lower() == 'f' else 'f'
